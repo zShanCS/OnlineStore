118da97c4d5b19c21d5b5754641792b04c61a5cc
zShan <73174295+zShanCS@users.noreply.github.com>
2021-01-11 19:47:20 +0500

    Update onlineStore.java

--- onlineStore.java
+++ onlineStore.java
@@ -3,15 +3,18 @@
 package assisgnment1;
 
 import java.io.Serializable;
+ //serialziable so I can convert each object into s string form for saving on file
 
 
 public class OnlineStore extends Store implements Serializable{
- //serialziable so I can convert each object into s string form for saving on file
+
+ //u can change this if mecessary
  static String webAddress="www.oop_mart_assignment1.com";
   static String  ID="ON1";
     public OnlineStore(){}
-    
+    //a default constructor
    
+ //method that returns data about store
     public  static void getStoreDetails(){
          System.out.println("Store ID \t       ;;   On1");
          System.out.println("Store Name        ;;   OO PMart");
